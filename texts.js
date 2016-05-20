ec15df4efb0b029c44aff304cf51704b58f148e0
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-05-20 17:21:02 +0700

    fix texts

--- texts.js
+++ texts.js
@@ -1,6 +1,12 @@
 module.exports = [
     {
-        text: 'управление регистрацией абонентов. чтобы зарегистрировать абонента нажмите 1, чтобы отрегистрировать абонента нажмите 0', 
+        text: [
+            'управление регистрацией абонентов.',
+            'чтобы зарегистрировать абонента нажмите 1,',
+            'чтобы отрегистрировать абонента нажмите 0,', 
+            'чтобы зарегистрироваться в очередях обслуживания нажмите 5',
+            'чтобы отрегистрироваться из очередей обслуживания нажмите 6'
+            ].join(' '), 
         file: 'sounds/hello.mp3'
     },
     {
@@ -12,23 +18,53 @@ module.exports = [
         file: 'sounds/inputpincode.mp3'
     },
     {
-        text: 'вы не являетесь оператором', 
-        file: 'sounds/you_are_not_operator.mp3'
+        text: 'вы не являетесь зарегистрированным абонентом', 
+        file: 'sounds/you_are_not_registered_user.mp3'
     },
     {
-        text: 'вы уже зарегистрированы', 
-        file: 'sounds/you_already_registred.mp3'
+        text: 'вы уже зарегистрированы как абонент', 
+        file: 'sounds/you_already_registred_user.mp3'
     },
     {
-        text: 'оператор уже зарегистрирован', 
+        text: 'абонент уже зарегистрирован', 
         file: 'sounds/operator_already_registred.mp3'
     },
     {
-        text: 'оператор отрегистрирован', 
-        file: 'sounds/operator_unregistrated.mp3'
+        text: 'абонент отрегистрирован', 
+        file: 'sounds/user_unregistrated.mp3'
     },
     {
-        text: 'оператор зарегистрирован', 
-        file: 'sounds/operator_registrated.mp3'
+        text: 'абонент зарегистрирован', 
+        file: 'sounds/user_registrated.mp3'
     },
+    {
+        text: 'абонент не состоит ни в одной очереди', 
+        file: 'sounds/user_not_in_queues.mp3'
+    },
+    {
+        text: 'абонент добавлен в очереди', 
+        file: 'sounds/user_add_in_queues.mp3'
+    },
+    {
+        text: 'абонент с этим пин-кодом уже зарегистрирован', 
+        file: 'sounds/user_already_registred.mp3'
+    },
+    {
+        text: 'на этот телефоне уже есть зарегистрированный абонент', 
+        file: 'sounds/user_already_registred_on_this_peer.mp3'
+    },
+    {
+        text: 'абонент зарегистрирован, в том числе и в очередях', 
+        file: 'sounds/user_registrated_in_queues.mp3'
+    },
+    {
+        text: 'абонент удален из очередей', 
+        file: 'sounds/user_removed_from_queues.mp3'
+    },
+    {
+        text: 'на этот телефоне нет зарегистрированного абонента', 
+        file: 'sounds/user_not_registered_on_this_peer.mp3'
+    }
+    
+    
 ];
