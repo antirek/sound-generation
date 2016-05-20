731efce145b4b6883a1f502a80e56c9f48add870
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-05-20 12:00:12 +0700

    change text

--- texts.js
+++ texts.js
@@ -1,18 +1,34 @@
 module.exports = [
     {
-    	text: 'один два три', 
-    	file: 'sounds/ten1.wav'
+        text: 'управление регистрацией абонентов. чтобы зарегистрировать абонента нажмите 1, чтобы отрегистрировать абонента нажмите 0', 
+        file: 'sounds/hello.mp3'
     },
     {
-    	text: 'один два три', 
-    	file: 'sounds/ten2.wav'
+        text: 'неверный пинкод', 
+        file: 'sounds/badpin.mp3'
     },
     {
-    	text: 'один два три', 
-    	file: 'sounds/ten3.wav'
+        text: 'введите пинкод', 
+        file: 'sounds/inputpincode.mp3'
     },
     {
-    	text: 'один два три', 
-    	file: 'sounds/ten4.wav'
-    }
+        text: 'вы не являетесь оператором', 
+        file: 'sounds/you_are_not_operator.mp3'
+    },
+    {
+        text: 'вы уже зарегистрированы', 
+        file: 'sounds/you_already_registred.mp3'
+    },
+    {
+        text: 'оператор уже зарегистрирован', 
+        file: 'sounds/operator_already_registred.mp3'
+    },
+    {
+        text: 'оператор отрегистрирован', 
+        file: 'sounds/operator_unregistrated.mp3'
+    },
+    {
+        text: 'оператор зарегистрирован', 
+        file: 'sounds/operator_registrated.mp3'
+    },
 ];
